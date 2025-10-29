f5352de6416f18c0de14525772359bfc156df679
suryakamal03 <professionalsurya3@gmail.com>
2025-10-29 20:01:58 +0530

    dict

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,31 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import  authRouter  from "./routes/Routesauth.js";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRouter from "./routes/Routesauth.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
 const app = express();
-dotenv.config()
-const PORT = process.env.Port || 5000
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/auth',authRouter)
+app.use("/api/auth", authRouter);
 
-app.listen(PORT,()=>{
-  console.log(`server is running ${PORT}`);
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
