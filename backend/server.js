42113e38bbf20a560aa1de97d6a912032ee5956c
suryakamal03 <professionalsurya3@gmail.com>
2025-10-31 19:27:04 +0530

    connected with mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import  authRouter  from "./routes/Routesauth.js";
+import {ConnectDB} from "./libs/db.js"
 const app = express();
 dotenv.config()
 const PORT = process.env.Port || 5000
@@ -11,4 +12,5 @@ app.use('/api/auth',authRouter)
 
 app.listen(PORT,()=>{
   console.log(`server is running ${PORT}`);
+  ConnectDB()
 })
