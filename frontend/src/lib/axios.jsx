054304b806b79f494e00ea21e57c2bc7c55fa1b1
suryakamal03 <professionalsurya3@gmail.com>
2025-11-28 11:36:00 +0530

    render url

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : "/api",
+  baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : "https://chat-application-kx10.onrender.com",
   withCredentials: true,
 })
