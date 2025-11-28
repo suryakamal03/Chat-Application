626059c1ed4be045ba95cba6d8406db5adbbac80
suryakamal03 <professionalsurya3@gmail.com>
2025-11-28 12:50:24 +0530

    jwt error

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken"
+
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
-    httpOnly: true, // prevents JS access to cookie (more secure)
-    sameSite: "strict", // protects against CSRF
-    secure: process.env.NODE_ENV === "production" //only HTTPS in prod 
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
   });
+
   return token;
 };
